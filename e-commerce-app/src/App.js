2fce2f35aadff15dc6221c603a483c2466cffcdf
bogo26 <florin.ionutas@gmail.com>
2023-08-04 12:15:45 +0300

    create navigation bar
    ~45 min

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -1,21 +1,22 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import AppRoutes from './Routes';
-import Navigation from './components/Navigation';
-import { fetchArticles } from './actions/articleActions';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import AppRoutes from "./Routes";
+import Navigation from "./components/Navigation";
+import { fetchArticles } from "./actions/articleActions";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // Fetch the articles when the component mounts
     dispatch(fetchArticles());
   }, [dispatch]);
 
-
   return (
     <div>
-      <Navigation />
+      <BrowserRouter>
+        <Navigation />
+      </BrowserRouter>
       <AppRoutes />
     </div>
   );
